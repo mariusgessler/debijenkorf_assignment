4af16be534750a5b9df4cbc301edde97e5dcff2b
mariusgessler <marius.gessler@gmail.com>
2020-11-07 18:51:01 +0100

    Add active state

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -6,14 +6,15 @@ import { ReactComponent as SearchIcon } from '../assets/icons/search.svg';
 
 const SearchBar = () => {
   const [query, setQuery] = useState('');
+  const [active, setActive] = useState(false);
   const handleSubmit = (e) => e.preventDefault();
 
   return (
   <div className="search-bar">
     <div className="search-bar__container">
-        <form className="search-bar__form" onSubmit={handleSubmit} >
+        <form className={`search-bar__form ${active ? 'active' : ''}`} onSubmit={handleSubmit} >
           <div className="search-bar__input-container">
-            <input className="search-bar__input" id="searchInput" type="search" placeholder="Zoeken" name="searchInput" aria-label="Search input" autoComplete="off" value={query} onChange={e => setQuery(e.target.value)}></input>
+            <input className="search-bar__input" id="searchInput" type="search" placeholder="Zoeken" name="searchInput" aria-label="Search input" autoComplete="off" value={query} onChange={e => setQuery(e.target.value)} onFocus={() => setActive(true)} onBlur={() => setActive(false)}></input>
             {query && <button className="search-bar__button search-bar__button--reset" value="Reset">
               <CloseIcon className="search-bar__icon search-bar__icon--reset" title="Reset search" onClick={() => setQuery('')}></CloseIcon>
             </button> }
