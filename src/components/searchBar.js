910d1855998ddacf940831d1f6dcb6bad216db3d
mariusgessler <marius.gessler@gmail.com>
2020-11-07 17:09:24 +0100

    Add search bar container element

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -3,8 +3,10 @@ import React from 'react';
 const SearchBar = () => {
   return (<div className="container container__search-bar">
     <form className="search-bar__form">
-      <input className="search-bar__input" id="searchInput" type="search" placeholder="Zoeken" name="searchInput" aria-label="Search input" autoComplete="off"></input>
-      <button className="search-bar__button--submit" id="searchButton" value="Search" type="submit"></button>
+      <div className="search-bar__input-container">
+        <input className="search-bar__input" id="searchInput" type="search" placeholder="Zoeken" name="searchInput" aria-label="Search input" autoComplete="off"></input>
+        <button className="search-bar__button--submit" id="searchButton" value="Search" type="submit">X</button>
+        </div>
     </form>
   </div>)
 } 
