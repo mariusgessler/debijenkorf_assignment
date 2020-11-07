4681a9ddc709dc425e3e958d99c5651abfa84c6a
mariusgessler <marius.gessler@gmail.com>
2020-11-07 18:00:36 +0100

    Fix import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SearchBar from './components/searchBar';
+import SearchBar from './components/SearchBar';
 import './styles/index.scss'
 
 const App = () => <SearchBar/>
