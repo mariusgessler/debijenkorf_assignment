85ebec066e96a3842e01f759c17777a2286a6581
mariusgessler <marius.gessler@gmail.com>
2020-11-07 18:24:05 +0100

    Add reset button

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,11 @@
-import React from 'react';
-// import { ReactComponent as CloseIcon } from '../assets/icons/close.svg';
+import React, { useState } from 'react';
+import { ReactComponent as CloseIcon } from '../assets/icons/close.svg';
 import { ReactComponent as SearchIcon } from '../assets/icons/search.svg';
 // import { ReactComponent as ApplyIcon } from '../assets/icons/diagonal-arrow.svg';
 
 
 const SearchBar = () => {
+  const [query, setQuery] = useState('');
   const handleSubmit = (e) => e.preventDefault();
 
   return (
@@ -12,8 +13,11 @@ const SearchBar = () => {
     <div className="search-bar__container">
         <form className="search-bar__form" onSubmit={handleSubmit} >
           <div className="search-bar__input-container">
-            <input className="search-bar__input" id="searchInput" type="search" placeholder="Zoeken" name="searchInput" aria-label="Search input" autoComplete="off"></input>
-            <button className="search-bar__button search-bar__button--search" id="searchButton" value="Search" type="submit">
+            <input className="search-bar__input" id="searchInput" type="search" placeholder="Zoeken" name="searchInput" aria-label="Search input" autoComplete="off" value={query} onChange={e => setQuery(e.target.value)}></input>
+            {query && <button className="search-bar__button search-bar__button--reset" value="Reset">
+              <CloseIcon className="search-bar__icon search-bar__icon--reset" title="Reset search" onClick={() => setQuery('')}></CloseIcon>
+            </button> }
+            <button className="search-bar__button search-bar__button--search" value="Search" type="submit">
               <SearchIcon className="search-bar__icon search-bar__icon--search" title="Search"></SearchIcon>
             </button>
           </div>
