985a7ec5cf175cc915a5b95f7976dfbbc242be64
mariusgessler <marius.gessler@gmail.com>
2020-11-07 15:49:39 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
+import SearchBar from './components/searchBar';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <h2>Hello world</h2>
-        </div>
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => <SearchBar/>
 
 export default App;
