7d45afc77143787c7d3c73a7eb16173337d8f80a
mariusgessler <marius.gessler@gmail.com>
2020-11-08 23:23:09 +0100

    Add tests for SearchSuggestions

---
+++ src/components/SearchSuggestions.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import SearchSuggestions from './SearchSuggestions';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const mockResponse = {
+  "suggestions": [
+    {
+       "searchterm": "heren jas",
+       "nrResults": 1100,
+    },
+    {
+       "searchterm":"dames jas",
+       "nrResults": 1501,
+    },
+    {
+       "searchterm":"kenzo jas",
+       "nrResults": 62,
+    },
+  ]
+};
+
+jest.mock('../hooks/useSuggestionsApi', () => {
+  return jest.fn(() => [{data: mockResponse}, jest.fn()]
+  );
+});
+
+describe('SearchSuggestions', () => {
+  const wrapper = shallow(<SearchSuggestions query="jas"/>);
+
+  it('correct amount of list-items gets rendered according to fetched data', () => {
+    expect(wrapper.find('.search-suggestions__list').children()).toHaveLength(mockResponse.suggestions.length);
+  });
+});
