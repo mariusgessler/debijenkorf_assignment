8831e337f7365267ea991cefcdab21a291c16d2b
mariusgessler <marius.gessler@gmail.com>
2020-11-08 12:48:39 +0100

    Fix active state on reset

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -19,26 +19,32 @@ const SearchBar = () => {
   useEffect(() => {
     if (input.length >= 2) {
       debounceSearchTerm(input);
+    } else {
+      setSearchTerm('')
     };
   }, [debounceSearchTerm, input]);
-
   return (
-  <div className="search-bar">
-    <div className="search-bar__container">
-        <form className={`search-bar__form ${active ? 'active' : 'inactive'}`} onSubmit={handleSubmit} >
-          <div className="search-bar__input-container">
-            <input className="search-bar__input" id="searchInput" type="search" placeholder="Zoeken" name="searchInput" aria-label="Search input" autoComplete="off" value={input} onChange={(e) => setInput(e.target.value)} onFocus={() => setActive(true)} onBlur={() => setActive(false)}></input>
-            {input && <button className="search-bar__button search-bar__button--reset" value="Reset">
-              <CloseIcon className="search-bar__icon search-bar__icon--reset" title="Reset search" onClick={() => setInput('')}></CloseIcon>
-            </button> }
-            <button className="search-bar__button search-bar__button--search" value="Search" type="submit">
-              <SearchIcon className="search-bar__icon search-bar__icon--search" title="Search"></SearchIcon>
-            </button>
-          </div>
-        </form>
+    <div className="search-component ">
+      <div className="search-bar">
+        <div className="search-bar__container">
+          <form className={`search-bar__form ${active ? 'active' : 'inactive'}`} onSubmit={handleSubmit} >
+            <div className="search-bar__input-container">
+              <input className="search-bar__input" id="searchInput" type="search" placeholder="Zoeken" name="searchInput" aria-label="Search input" autoComplete="off" value={input} onChange={(e) => setInput(e.target.value)} onFocus={() => setActive(true)} onBlur={() =>  setActive(false)}></input>
+              {input && <button className="search-bar__button search-bar__button--reset" value="Reset" onClick={() => {
+                setInput('') 
+                setActive(false)}}>
+                <CloseIcon className="search-bar__icon search-bar__icon--reset" title="Reset search" ></CloseIcon>
+              </button> }
+              <button className="search-bar__button search-bar__button--search" value="Search" type="submit">
+                <SearchIcon className="search-bar__icon search-bar__icon--search" title="Search"></SearchIcon>
+              </button>
+            </div>
+          </form>
+        </div>
+        {searchTerm && <SearchSuggestions query={searchTerm} setQuery={setInput}/> }
       </div>
-      <SearchSuggestions query={searchTerm} setSearchTerm={setSearchTerm}/>
-  </div>
+    </div>
+  
  );
 };
 
