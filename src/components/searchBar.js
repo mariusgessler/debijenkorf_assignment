b54ae435a19299a675e7322ac2cf01f4adfc955b
mariusgessler <marius.gessler@gmail.com>
2020-11-08 11:27:27 +0100

    Add debounce function

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,22 +1,35 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { ReactComponent as CloseIcon } from '../assets/icons/close.svg';
 import { ReactComponent as SearchIcon } from '../assets/icons/search.svg';
-// import { ReactComponent as ApplyIcon } from '../assets/icons/diagonal-arrow.svg';
-
+import SearchSuggestions from './SearchSuggestions';
+import debounce from 'lodash.debounce';
 
 const SearchBar = () => {
-  const [query, setQuery] = useState('');
+  const [input, setInput] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   const [active, setActive] = useState(false);
   const handleSubmit = (e) => e.preventDefault();
+
+  const debounceSearchTerm = useCallback(
+    debounce((value) => {
+      setSearchTerm(value);
+    }, 500), [],
+  );
+
+  useEffect(() => {
+    if (input.length >= 2) {
+      debounceSearchTerm(input);
+    };
+  }, [debounceSearchTerm, input]);
 
   return (
   <div className="search-bar">
     <div className="search-bar__container">
-        <form className={`search-bar__form ${active ? 'active' : ''}`} onSubmit={handleSubmit} >
+        <form className={`search-bar__form ${active ? 'active' : 'inactive'}`} onSubmit={handleSubmit} >
           <div className="search-bar__input-container">
-            <input className="search-bar__input" id="searchInput" type="search" placeholder="Zoeken" name="searchInput" aria-label="Search input" autoComplete="off" value={query} onChange={e => setQuery(e.target.value)} onFocus={() => setActive(true)} onBlur={() => setActive(false)}></input>
-            {query && <button className="search-bar__button search-bar__button--reset" value="Reset">
-              <CloseIcon className="search-bar__icon search-bar__icon--reset" title="Reset search" onClick={() => setQuery('')}></CloseIcon>
+            <input className="search-bar__input" id="searchInput" type="search" placeholder="Zoeken" name="searchInput" aria-label="Search input" autoComplete="off" value={input} onChange={(e) => setInput(e.target.value)} onFocus={() => setActive(true)} onBlur={() => setActive(false)}></input>
+            {input && <button className="search-bar__button search-bar__button--reset" value="Reset">
+              <CloseIcon className="search-bar__icon search-bar__icon--reset" title="Reset search" onClick={() => setInput('')}></CloseIcon>
             </button> }
             <button className="search-bar__button search-bar__button--search" value="Search" type="submit">
               <SearchIcon className="search-bar__icon search-bar__icon--search" title="Search"></SearchIcon>
@@ -24,6 +37,7 @@ const SearchBar = () => {
           </div>
         </form>
       </div>
+      <SearchSuggestions query={searchTerm} setSearchTerm={setSearchTerm}/>
   </div>
  );
 };
